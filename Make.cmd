9fa83cd0320298d5dbbb54b48f1689cc1d8a4f3f
Thomas Lenzi <tlenzi@ulb.ac.be>
2016-06-14 09:09:51 +0200

    Quick save at `date -u`

---
+++ Make.cmd
@@ -0,0 +1,5 @@
+pdflatex thesis
+bibtex thesis
+pdflatex thesis
+pdflatex thesis
+rm thesis-blx.bib thesis.aux thesis.bbl thesis.blg thesis.log thesis.lof thesis.lot thesis.out thesis.run.xml thesis.toc thesis.bcf feynman-draw.* feyngraph.* diagram.pdf diagram.png
---
+++ Save.cmd
@@ -0,0 +1,5 @@
+git add .
+git rmd
+git commit -m "Quick save at `date -u`"
+git push origin
+git push iihe-daq
