f9e342f221b443efd1dfc8ead7f22a2bf607f42e
Thomas Lenzi <tlenzi@ulb.ac.be>
2016-08-29 20:09:30 +0200

    Quick save at Mon Aug 29 18:09:30 UTC 2016

--- tools/import_plots.cpp
+++ tools/import_plots.cpp
@@ -9,5 +9,16 @@
       TCanvas* c = (TCanvas*) key->ReadObj();
       c->SaveAs(".pdf");
    }
+
+   TFile* f2(TFile::Open("/Users/tlenzi/Documents/PhD/software/Irradiation/plot.root"));
+   TIter nex2(f2->GetListOfKeys());
+   TKey* key2;
+   while ((key2 = (TKey*) nex2())) {
+      TClass *cl = gROOT->GetClass(key2->GetClassName());
+      if (!cl->InheritsFrom("TCanvas")) continue;
+      TCanvas* c = (TCanvas*) key2->ReadObj();
+      c->SaveAs(".pdf");
+   }
+
    return 0;
 }
